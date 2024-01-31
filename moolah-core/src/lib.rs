528e65224bae8fe00906f3ba89827446520a4d3a
kevin ringier <kevinringier@pop-os.localdomain>
2024-01-30 23:11:55 -0700

    initial commit

---
+++ moolah-core/src/lib.rs
@@ -0,0 +1,13 @@
+// TODO:
+// present value of a cash flow
+// future value of a cash flow
+// ACF annuity
+// ADF annuity
+// present value of an annuity
+// future value of an annuity
+// ACF perpetuity
+// ADF perpetuity
+// present value of a perpetuity
+// future value of a perpetuity
+#[cfg(test)]
+mod tests {}
